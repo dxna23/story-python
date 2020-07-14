47ee46407a426b3df6b3953d900f0ae6acf8419a
Chandana Akasapu <63527073+dxna23@users.noreply.github.com>
2020-07-13 23:31:23 -0500

    Add files via upload

---
+++ story.py
@@ -0,0 +1,7 @@
+personName = input("Enter a name: ")
+anObject = input("Enter an object: ")
+place = input("Enter a place: ")
+story = personName + " was walking through " + place + ". " + place + " was not usually very interesting. " + personName + " spotted a small " + anObject + ". Suddenly the " + anObject + " jumped up and ran away. "+ personName + " decided not to go " + place + " again."
+print (story)
+
+
